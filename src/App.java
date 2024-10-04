f13ab61aa5f305fba25057a50a78983922632ddc
Fantasm4no <erickzhigue@gmail.com>
2024-10-04 12:05:55 -0500

    Práctica 04/10/24

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         OrdenamientoBurbujaAvanzado ordenar = new OrdenamientoBurbujaAvanzado();
-        int [] arreglo = {5, 8, 9, 0, 2};
+        int[] arreglo = {0, 2, 4, 6, 8};
         int[] arregloBubble = ordenar.sortBubbleAvanzado(arreglo);
         ordenar.printBubbleAvanzado(arregloBubble);
     }
--- src/OrdenamientoBurbujaAvanzado.java
+++ src/OrdenamientoBurbujaAvanzado.java
@@ -1,15 +1,26 @@
 public class OrdenamientoBurbujaAvanzado {
     public int[] sortBubbleAvanzado(int[] arreglo){
         int n = arreglo.length;
+        boolean intercambio = false;
         for(int i = 0; i < n; i++){
+            intercambio = false;
+            System.out.println("Pasada: " + i);
             for(int j = 0; j < n - 1 - i; j++){
+                System.out.println("j= " + j +" [j]=" + arreglo[j] + 
+                " j+1= " + j+1 +" [j+1]=" + arreglo[j+1]);
                 if(arreglo[j] > arreglo[j + 1] ){
-                //Intercambio
-                int aux = arreglo[j];
-                arreglo[j] = arreglo[j + 1];
-                arreglo[j + 1] = aux;
-                }
+                    System.out.println("Si hay cambio");
+                    //Intercambio
+                    int aux = arreglo[j];
+                    arreglo[j] = arreglo[j + 1];
+                    arreglo[j + 1] = aux;
+                    intercambio = true;
+                } 
+            //Si no hubo ningun intercambio, es porque ya esta ordenado y no deberia preguntar más.
+            if(!intercambio){
+                break;
             }
+            }          
         }
         return arreglo;
     }
@@ -17,5 +28,5 @@ public class OrdenamientoBurbujaAvanzado {
         for( int elemento : arreglo){
             System.out.print(elemento + ", ");
         }
-    }   
-}
+    }
+}    
