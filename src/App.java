eb2682833cab9bc7eba80b1fb3ecc53315968c78
Fantasm4no <erickzhigue@gmail.com>
2024-10-04 13:02:15 -0500

    Práctiva Viernes 04

--- src/App.java
+++ src/App.java
@@ -1,40 +1,37 @@
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        OrdenamientoBurbujaAvanzado ordenar = new OrdenamientoBurbujaAvanzado();
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese el tamaño del arreglo; ");
-        int tamanio = 0;
-
-        do{
-            System.out.println("Ingrese el tamaño con un entero positivo");
-            while(!scanner.hasNextInt()){
-                System.out.println("Le pedí un entero válido");
-                scanner.next();
-            }
-            tamanio = scanner.nextInt();
-            if(tamanio <= 0){
-                System.out.println("El tamaño debe ser entero positivo");
-
-            }
-        } while(tamanio <= 0);
+        int tamanio = leerEnteroValido(scanner, "Ingrese el tamaño del arreglo: ", false);
+        //Crear arreglo
         int[] arreglo = new int[tamanio];
 
-        System.out.println("Ingrese los valores del arreglo: ");
-
         for(int i = 0; i < tamanio; i++){
-            System.out.println("Ingrese los valores en la posición: " + i);
-            int numero = scanner.nextInt();
-            arreglo[i] = numero;
+            //Validar cada elemento del arreglo
+            arreglo[i] = leerEnteroValido(scanner, "Elemento" + (i + 1) + ":", true);
         }
-        System.out.print("[");
-        for(int j = 0; j< tamanio; j++){
-            System.out.print(" " + arreglo[j]);
+        for(int i : arreglo){
+            System.out.print(i);
         }
-        System.out.print(" ]");
-        
-        
         scanner.close();
     }
+    public static int leerEnteroValido(Scanner scanner, String mensaje, boolean numerosNegativos){
+
+        int numero;
+        do{
+            System.out.println(mensaje);
+            while(!scanner.hasNextInt()){
+                System.out.println("Le pedí un entero válido");
+                System.out.println(mensaje);
+                scanner.next();
+            }
+            numero = scanner.nextInt();
+            if(numerosNegativos && numero < 0){
+                System.out.println("No se permiten numeros negativos");
+
+            }
+        } while(numerosNegativos && numero < 0);
+        return numero;
+    }    
 }
