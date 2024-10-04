2324b4d06465cd701f6c551c4993cd96338f224e
Fantasm4no <erickzhigue@gmail.com>
2024-10-04 12:24:35 -0500

    Práctica Viernes 4

--- src/App.java
+++ src/App.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         OrdenamientoBurbujaAvanzado ordenar = new OrdenamientoBurbujaAvanzado();
-        int[] arreglo = {0, 2, 4, 6, 8};
-        int[] arregloBubble = ordenar.sortBubbleAvanzado(arreglo);
-        ordenar.printBubbleAvanzado(arregloBubble);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el tamaño del arreglo; ");
+        int tamanio = 0;
+
+        do{
+            System.out.println("Ingrese el tamaño con un entero positivo");
+            while(!scanner.hasNextInt()){
+                System.out.println("Le pedí un entero válido");
+                scanner.next();
+            }
+            tamanio = scanner.nextInt();
+            if(tamanio <= 0){
+                System.out.println("El tamaño debe ser entero positivo");
+
+            }
+        } while(tamanio <= 0);
+        int[] arreglo = new int[tamanio];
+
+        System.out.println("Ingrese los valores del arreglo: ");
+
+        scanner.close();
     }
 }
